99d3ff6d85e81029878052eea22247ae05c3d42d
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-10-13 13:51:29 +0400

    Add i3 config

---
+++ i3/scripts/display_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xrandr --output DVI-D-0 --auto --right-of HDMI-A-0 --rotate right
+xrandr --output HDMI-A-0 --mode 3440x1440 --rate 99.98
+
+feh --bg-fill /home/shlyapp/Pictures/xAFsBlx8.png
